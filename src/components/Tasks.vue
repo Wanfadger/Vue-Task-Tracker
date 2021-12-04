9c5ae8b4584418f257d873a8cc557b22ada49c09
wanfadger <galiwango.fahad@gmail.com>
2021-12-04 15:54:15 +0300

    create demo tasks data
    added tasks component to manage all tasks
    
    added task component to manage single task
    
    added fontawesome cdn link from cdnjs.com

---
+++ src/components/Tasks.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <Task :task="task" />
+    </div>
+</template>
+
+
+<script>
+import Task from './Task.vue'
+
+export default {
+name:"Tasks",
+components:{
+    Task
+},
+props:{
+    tasks:{
+        default:[]
+    }
+}  
+}
+</script>
+
+
+<style scoped>
+
+</style>
