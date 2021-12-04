119d7ca8e3851091682354e6fcb8ec7a18cc47f1
wanfadger <galiwango.fahad@gmail.com>
2021-12-04 15:16:50 +0300

    initialised task tracker using vue cli
    added header and button components

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+<header>
+<h1>{{title}}</h1>
+<Button text="Add Task" color="green" />
+</header>
+</template>
+
+
+<script>
+import Button from './Button'
+
+
+
+export default {        
+    name:"Header",
+    components:{
+        Button
+        
+    },
+    props:{
+        title:{
+            default:"Hello World",
+            type:String
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+ header{
+     display: flex;
+     justify-content: space-between;
+     margin-bottom: 20px;
+     align-items: center;
+
+ }
+</style>
